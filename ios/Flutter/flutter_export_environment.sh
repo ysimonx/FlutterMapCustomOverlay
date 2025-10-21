3069e5920f8f2137671f82c00952e3822224b635
Yannick Simon <ysimonx@Mac-mini-de-Yannick.local>
2025-10-21 08:51:41 +0200

    refactoring

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/ysimonx/Developpement/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ysimonx/Developpement/FlutterMapCustomOverlay"
+export "FLUTTER_ROOT=/Volumes/acasis/Users/ysimonx/Developpement/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/acasis/Users/ysimonx/Developpement/FlutterMapCustomOverlay"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
